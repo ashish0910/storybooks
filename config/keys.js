4e11fae7d960b18afbdb4cbe0713750eb29efad4
ashish aggarwal <ashish09101998@gmail.com>
2018-04-03 09:10:34 +0530

    Source code

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+    mongoURI:'mongodb://ashish:ashish@ds117489.mlab.com:17489/storybooks-dev',
+    googleClientID:'577913340901-lp3b7vp3b600bkiab8fnhqak6bumdiik.apps.googleusercontent.com',
+    googleClientSecret:'e1lFiDzUp87h5LjWJkZSPRWq'
+}
